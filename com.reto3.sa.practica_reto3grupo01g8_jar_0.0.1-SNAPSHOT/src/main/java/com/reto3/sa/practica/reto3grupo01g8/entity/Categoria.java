4b21c3df95d5035ab39b54a3e43113c416857a47
Harold <harold.rodriguez.mt@correo.usa.edu.co>
2021-10-07 21:34:10 -0500

    07/10/2021

---
+++ com.reto3.sa.practica_reto3grupo01g8_jar_0.0.1-SNAPSHOT/src/main/java/com/reto3/sa/practica/reto3grupo01g8/entity/Categoria.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.reto3.sa.practica.reto3grupo01g8.entity;
+
+/**
+ *
+ * @author Har
+ */
+public class Categoria {
+    
+}
