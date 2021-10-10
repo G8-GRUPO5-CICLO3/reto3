38c52262cf58865a9fc87faf677f3f7090e1101c
edgarsanchez1mt <edgar.sanchez1.mt@correo.usa.edu.co>
2021-10-10 13:43:48 -0500

    final

---
+++ com.reto3.sa.practica_reto3grupo01g8_jar_0.0.1-SNAPSHOT/src/main/java/com/reto3/sa/practica/reto3grupo01g8/controller/MessageController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.reto3.sa.practica.reto3grupo01g8.controller;
+
+import com.reto3.sa.practica.reto3grupo01g8.entity.Message;
+import com.reto3.sa.practica.reto3grupo01g8.service.MessageService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Edgar Javier Sanchez
+ */
+@RestController
+@RequestMapping("/api/Message/")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+public class MessageController {
+    @Autowired
+    private MessageService messageService;
+    
+    @GetMapping("/all")
+    public  List<Message> getMessage(){
+        return messageService.getMessageAll();
+        
+    }
+    @GetMapping("id")
+    public Optional<Message>getMessagexId(@PathVariable("id") int idMessage){
+        return messageService.getMessagexId(idMessage);
+    }  
+    
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Message save(@RequestBody Message category) {
+        return messageService.save(message);
+    }
+}
