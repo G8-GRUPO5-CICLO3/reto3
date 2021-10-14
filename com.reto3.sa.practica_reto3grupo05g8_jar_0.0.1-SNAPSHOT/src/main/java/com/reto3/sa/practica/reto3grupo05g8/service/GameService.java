4a45459d44031f4cd1802bf2ec63aa1895b74323
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2021-10-13 20:32:04 -0500

    cambios

--- com.reto3.sa.practica_reto3grupo05g8_jar_0.0.1-SNAPSHOT/src/main/java/com/reto3/sa/practica/reto3grupo05g8/service/GameService.java
+++ com.reto3.sa.practica_reto3grupo05g8_jar_0.0.1-SNAPSHOT/src/main/java/com/reto3/sa/practica/reto3grupo05g8/service/GameService.java
@@ -17,31 +17,19 @@ public class GameService {
   
     @Autowired
     private GameRepository repo;
-    
-<<<<<<< HEAD:com.reto3.sa.practica_reto3grupo05g8_jar_0.0.1-SNAPSHOT/src/main/java/com/reto3/sa/practica/reto3grupo05g8/service/GameService.java
-    public List<Game> listGame(){
-=======
-    public Games insertGames(Games game){
+
+    public Game insertGames(Game game){
         return repo.save(game);
     }
     
-    public List<Games> listGame(){
->>>>>>> 7a0cb531dff717f06508aa92d22c0d764dfe973f:com.reto3.sa.practica_reto3grupo05g8_jar_0.0.1-SNAPSHOT/src/main/java/com/reto3/sa/practica/reto3grupo05g8/service/GamesService.java
+    public List<Game> listGame(){
         return repo.findAll();
     }
     
     public Game listGameById(Game game){
         return repo.findById(game.getId()).orElse(null);
     }
-    
-<<<<<<< HEAD:com.reto3.sa.practica_reto3grupo05g8_jar_0.0.1-SNAPSHOT/src/main/java/com/reto3/sa/practica/reto3grupo05g8/service/GameService.java
-    public Game insertGames(Game game){
-        return repo.save(game);
-    }
-=======
-    
->>>>>>> 7a0cb531dff717f06508aa92d22c0d764dfe973f:com.reto3.sa.practica_reto3grupo05g8_jar_0.0.1-SNAPSHOT/src/main/java/com/reto3/sa/practica/reto3grupo05g8/service/GamesService.java
-      
+   
     public Game updtGames(Game game){
         Game checkIfExist = repo.findById(game.getId()).orElse(null);
         checkIfExist.setDeveloper   (game.getDeveloper());
